e978064ce4a037d838abd913c3b23789f82ee408
d-otis <dan@dan-foley.com>
2020-04-19 17:27:18 -0400

    add user model + db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_202148) do
+ActiveRecord::Schema.define(version: 2020_04_19_212156) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 2020_04_19_202148) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
