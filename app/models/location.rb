ef3bf5ea7c2300365df7197f9dc853649389e04f
d-otis <dan@dan-foley.com>
2020-04-19 18:43:39 -0400

    add slug capabiities to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,12 @@ class Location < ActiveRecord::Base
 	has_many :location_items
 	has_many :items, through: :location_items
 
+	def slug
+		name.downcase.gsub(/[!?@ +,&]/, "-").gsub(/[().']/, "").gsub("$", "s").squeeze("-")
+	end
+
+	def find_by_slug(slug)
+		self.all.find {|instance| instance.slug == slug} 
+	end
+
 end
