46a7d1944d6c0e3d60f27fd545b47236f73c440f
d-otis <dan@dan-foley.com>
2020-04-19 19:45:35 -0400

    define patch route for location editing

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,9 +6,23 @@ class LocationsController < ApplicationController
 		erb :"/locations/index"
 	end
 
+	get "/locations/:slug/edit" do
+		@location = Location.find_by_slug(params[:slug])
+		@items = Item.order(name: :asc)
+
+		erb :"/locations/edit"
+	end
+
 	get "/locations/:slug" do
 		@location = Location.find_by_slug(params[:slug])
 
 		erb :"/locations/show"
+	end
+
+	patch "/locations/:slug" do
+		location = Location.find_by_slug(params[:slug])
+		location.update(params[:location])
+
+		redirect "/locations/#{location.slug}"
 	end
 end
