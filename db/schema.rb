5f5fa07c5c09c32469bf1a4414e3298a5172ed11
d-otis <dan@dan-foley.com>
2020-04-20 17:45:39 -0400

    schema update from migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_175059) do
+ActiveRecord::Schema.define(version: 2020_04_20_213428) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_20_175059) do
   create_table "location_items", force: :cascade do |t|
     t.integer "location_id"
     t.integer "item_id"
-    t.float "item_count"
-    t.float "item_par"
+    t.float "item_count", default: 0.0
+    t.float "item_par", default: 0.0
     t.boolean "under"
   end
 
