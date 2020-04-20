7a356b5ae6f678eda68f998966e1c9e05c3f1e91
d-otis <dan@dan-foley.com>
2020-04-20 18:22:27 -0400

    define helper method for automatic update of UNDER boolean located in patch route for location updates

--- app/models/location_item.rb
+++ app/models/location_item.rb
@@ -1,4 +1,9 @@
 class LocationItem < ActiveRecord::Base
 	belongs_to :item
 	belongs_to :location
+
+	def self.update_under_status_all
+		LocationItem.where("item_count < item_par").update_all(under: true)
+		LocationItem.where("item_count >= item_par").update_all(under: false)
+	end
 end
