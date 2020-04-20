4b07426d9877fc47109a7349afc3aa7e5639a4d9
d-otis <dan@dan-foley.com>
2020-04-20 13:55:29 -0400

    schema update to float for item_count/par

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_212156) do
+ActiveRecord::Schema.define(version: 2020_04_20_175059) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_04_19_212156) do
   create_table "location_items", force: :cascade do |t|
     t.integer "location_id"
     t.integer "item_id"
-    t.integer "item_count"
-    t.integer "item_par"
+    t.float "item_count"
+    t.float "item_par"
     t.boolean "under"
   end
 
