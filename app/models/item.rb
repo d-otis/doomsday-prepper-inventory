39dc57387863a8de2eb0f0ac58384b93f4abc37e
d-otis <dan@dan-foley.com>
2020-04-20 12:33:47 -0400

    add helper methods for slugging and finding an item's count + par

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,28 @@ class Item < ActiveRecord::Base
 
 	validates :name, presence: true
 
+	def slug
+		name.downcase.gsub(/[!?@ +,&]/, "-").gsub(/[().']/, "").gsub("$", "s").squeeze("-")
+	end
+
+	def self.find_by_slug(slug)
+		self.all.find {|instance| instance.slug == slug} 
+	end
+
+	def item_count(location)
+		if self.locations.include?(location)
+			self.location_items.find_by(location: location).item_count
+		end
+	end
+
+	def item_par(location)
+		self.location_items.find_by(location: location)&.item_par
+	end
+
+	def li_id(item_id, location_id) 
+		if self.location_items.find_by(item_id: item_id, location_id: location_id)
+			self.location_items.find_by(item_id: item_id, location_id: location_id).id
+		end
+	end
+
 end
