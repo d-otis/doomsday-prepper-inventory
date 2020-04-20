0aa6b946b8ca01c2fe31711ab9276d4434886c05
d-otis <dan@dan-foley.com>
2020-04-20 12:31:49 -0400

    implement location_item join table editing capabilities

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,6 +8,7 @@ class LocationsController < ApplicationController
 
 	get "/locations/:slug/edit" do
 		@location = Location.find_by_slug(params[:slug])
+		@location_items = LocationItem.where(location: @location)
 		@items = Item.order(name: :asc)
 
 		erb :"/locations/edit"
@@ -22,6 +23,12 @@ class LocationsController < ApplicationController
 	patch "/locations/:slug" do
 		location = Location.find_by_slug(params[:slug])
 		location.update(params[:location])
+		
+		params[:location_items].first.each do |l_i|
+			if l_i.first.to_i > 0 && !l_i.last.values.any?(&:empty?)
+				LocationItem.find(l_i.first).update(l_i.last)
+			end
+		end
 
 		redirect "/locations/#{location.slug}"
 	end
