0f031c60afced78b3e426effff156521d55417db
d-otis <dan@dan-foley.com>
2020-04-21 13:45:47 -0400

    #need helper method for location-specific inventory needs

--- app/models/location_item.rb
+++ app/models/location_item.rb
@@ -6,4 +6,9 @@ class LocationItem < ActiveRecord::Base
 		LocationItem.where("item_count < item_par").update_all(under: true)
 		LocationItem.where("item_count >= item_par").update_all(under: false)
 	end
+
+	def need
+		result = self.item_par - self.item_count
+		result > 0 ? result : "<span class='badge badge-pill badge-success'>Stocked</span>"
+	end
 end
