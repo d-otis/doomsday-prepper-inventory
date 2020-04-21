0e219323d2b42d6e14a8a63c64c772df4e074251
d-otis <dan@dan-foley.com>
2020-04-21 15:16:16 -0400

    add session helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,14 @@ class ApplicationController < Sinatra::Base
 		redirect "/items"
 	end
 
+	helpers do
+		def logged_in?
+			!!current_user
+		end
+
+		def current_user
+			@current_user ||= session[:user_id]
+		end
+	end
+
 end
