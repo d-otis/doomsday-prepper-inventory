7356cf505a33b5e5ce21ad199614919bdae43088
d-otis <dan@dan-foley.com>
2020-04-22 17:23:52 -0400

    change back to RACK_ENV

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
-ENV['SINATRA_ENV'] ||= "production"
+ENV['RACK_ENV'] ||= "production"
 
 require_relative "./config/environment"
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.require
 
-ENV['SINATRA_ENV'] ||= "production"
+ENV['RACK_ENV'] ||= "production"
 
 # ActiveRecord::Base.establish_connection(
 #   :adapter => "sqlite3",
