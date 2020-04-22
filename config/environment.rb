8f9d930b7969f04c7629bf766052f66be4022234
d-otis <dan@dan-foley.com>
2020-04-22 14:49:55 -0400

    comment out sqlite db connection setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 require 'bundler/setup'
 Bundler.require
 
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/inventory_#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/inventory_#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 require_relative "session_secret"
 
